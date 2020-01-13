b4700a98a3306c7d0713019d13943bbb1c226ab8
zwan2016 <zwan0929@gmail.com>
2020-01-13 16:03:03 -0500

    add user domain for registration model

---
+++ samples/oauth2/sparklr/src/main/java/org/springframework/security/oauth/examples/sparklr/domain/User.java
@@ -0,0 +1,62 @@
+package org.springframework.security.oauth.examples.sparklr.domain;
+
+/**
+ * Created by zwan on 1/13/20.
+ */
+
+public class User {
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String funFact;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFunFact() {
+        return funFact;
+    }
+
+    public void setFunFact(String funFact) {
+        this.funFact = funFact;
+    }
+}
