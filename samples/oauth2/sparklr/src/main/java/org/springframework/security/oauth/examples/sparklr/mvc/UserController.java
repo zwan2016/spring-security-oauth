e7925b2850362e00fb96a493e3bbaa589de3c30b
zwan2016 <zwan0929@gmail.com>
2020-01-14 17:54:16 -0500

    postgres support

--- samples/oauth2/sparklr/src/main/java/org/springframework/security/oauth/examples/sparklr/mvc/UserController.java
+++ samples/oauth2/sparklr/src/main/java/org/springframework/security/oauth/examples/sparklr/mvc/UserController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth.examples.sparklr.PhotoServiceUser;
 import org.springframework.security.oauth.examples.sparklr.domain.User;
+import org.springframework.security.oauth.examples.sparklr.repository.UserRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
@@ -15,9 +16,16 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class UserController {
 
+	@Autowired
+	UserRepository userRepository;
+
 	@GetMapping("/test")
 	public ModelAndView register()
 	{
+		User user = new User();
+		user.setUsername("zwan");
+		user.setPassword("Mstr123");
+		userRepository.save(user);
 		return new ModelAndView("oauth_error");
 	}
 }
---
+++ samples/oauth2/sparklr/src/main/java/org/springframework/security/oauth/examples/sparklr/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package org.springframework.security.oauth.examples.sparklr.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.oauth.examples.sparklr.domain.User;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by zwan on 1/14/20.
+ */
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
